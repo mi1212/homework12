e89b003c0f27237755d460f6fdb11ecfb057f11b
mi1212 <mc_gorba4ev@mail.ru>
2023-01-24 11:43:54 +0300

    added project

---
+++ homework12/homework12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homework12
+//
+//  Created by Mikhail Chuparnov on 24.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
